279b3cb0643dcd2c20dafda5019e8595167b84dd
FiggyFoxFunc <131496393+jack-adorbs@users.noreply.github.com>
2025-05-20 12:42:36 +0100

    Setting up site for boids demo.

---
+++ index.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(200, 200, document.getElementById("DemoCanvas"));
+
+  background(0);
+  stroke("blueviolet");
+  strokeWeight(1);
+}
+
+function draw() {
+  background(0);
+}
